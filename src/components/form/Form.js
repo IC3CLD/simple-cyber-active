0047ec1787bfb8360f35c83091e75d6b73903faf
IC3CLD <67324553+IC3CLD@users.noreply.github.com>
2021-06-27 22:02:37 +0300

    input anim

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,36 +1,38 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
 import { init } from "emailjs-com";
+import Loader from "react-loader-spinner";
+import vector from "../../images/Vector.svg";
 
 const Form = () => {
   init("user_k6lM6BhZozsGS99tvHSDe");
   const [submitted, setSubmitted] = useState(false);
   const [value, setValue] = useState("");
+  const [load, setLoad] = useState(false);
 
   const handleChange = (e) => {
-    setValue(e.target.value)
-  }
-  
+    setValue(e.target.value);
+  };
+
   function sendEmail(e) {
     e.preventDefault();
+    setLoad(true);
 
-    emailjs
-      .sendForm(
-        "service_1r4qvhw",
-        "template_8x3q9n6",
-        e.target,
-        "user_k6lM6BhZozsGS99tvHSDe"
-      )
-      .then(
-        (result) => {
-          setSubmitted(true)
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    e.target.reset();
+    // emailjs
+    //   .sendForm(
+    //     "service_1r4qvhw",
+    //     "template_8x3q9n6",
+    //     e.target,
+    //     "user_k6lM6BhZozsGS99tvHSDe"
+    //   )
+    //   .then(
+    //     (result) => {},
+    //     (error) => {}
+    //   );
+    // e.target.reset();
+    setInterval(() => {
+      setSubmitted(true);
+    }, 2000);
   }
 
   return (
@@ -43,19 +45,34 @@ const Form = () => {
           <span className={"form-submitted"}>
             Thanks - We'll keep in touch 👍
           </span>
-        ) : null}
-        <input
-          autoComplete={"off"}
-          className={"form-input"}
-          type="email"
-          placeholder="Your email"
-          name="email"
-          value={value}
-          onChange={handleChange}
-        />
-        <button className={`form-btn`} type="submit" disabled={!value}>
-          <span className={"form-btn-text"}>Let me know</span>
-        </button>
+        ) : (
+          <>
+            <input
+              autoComplete={"off"}
+              className={"form-input"}
+              type="email"
+              placeholder="Your email"
+              name="email"
+              value={value}
+              onChange={handleChange}
+            />
+            <button className={`form-btn`} type="submit" disabled={!value}>
+              {!load && <span className={"form-btn-text"}>Let me know</span>}
+              {!load && (
+                <img src={vector} alt="arrow" className={"form-btn-vector"} />
+              )}
+
+              {load && (
+                <Loader
+                  type="TailSpin"
+                  color="#FFFFFF"
+                  height={40}
+                  width={40}
+                />
+              )}
+            </button>
+          </>
+        )}
       </form>
     </section>
   );
